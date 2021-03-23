813701f041bf6c1368c90f282fdbfdc695db397c
imbrikis <imbrikis@yahoo.com>
2021-03-23 11:09:14 -0600

    styling: layout

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import NumGenerator from './components/NumGenerator'
 const App = () => {
   return (
     <div className='app-container'>
-      <h1>Why is data visualization so cool?</h1>
-      <h1>Let's find out!</h1>
-      <h2>Find the nines</h2>
+      <h1 className='mt-12'>Why is data visualization so cool?</h1>
+      <h1 className='mb-6'>Let's find out!</h1>
+      <h2 className='mb-2'>Find the nines</h2>
       <NumGenerator />
       {/* <NumSelector /> */}
+      <h2 className='mt-12 mb-2'>How about we add some helpers?</h2>
+      <NumGenerator circleTheNines />
+      <h2 className='mt-12'>That still seems to take a while</h2>
+      <h2 className='mb-6'>
+        We can do better... with{' '}
+        <span className='i-400'>Data Visualization</span>!
+      </h2>
     </div>
   )
 }
